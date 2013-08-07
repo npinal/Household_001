614f216c812a7b2be046283497f41cedaf1455df
npinal <jeanclad@npinal.com>
2013-08-07 21:02:15 +0900

    1. Handler test

--- Grip/src/com/dmc/grip/setting/GripActivity.java
+++ Grip/src/com/dmc/grip/setting/GripActivity.java
@@ -161,6 +161,8 @@ public class GripActivity extends Activity {
 		Log.e(TAG, "onPause");
 		
 		handler.removeMessages(0);
+		
+		mGripSensorEventManager.removeCollectSensorDataHandler();
 		mGripSensorEventManager.unregisterCAListener();
 	}
 
